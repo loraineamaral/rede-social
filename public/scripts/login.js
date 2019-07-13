2709d638f0e1fc9f628bd68eaf13736f7218ab30
loraineamaral <loraineeamaral@gmail.com>
2019-07-13 16:31:39 -0300

    cprreção sign in facebook

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,10 +1,16 @@
 $(document).ready(function () {
 
+    $(document).keypress(function (e) {
+        if (e.which == 13) {
+            $("#btn-login").trigger('click');
+        }
+    });
+
     $("#btn-login").click(function (event) {
         event.preventDefault();
         let email = $("#email").val();
         let password = $("#password").val();
-    
+
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(function (response) {
                 window.location = 'profile.html?id=' + response.user.uid;
@@ -12,43 +18,36 @@ $(document).ready(function () {
             .catch(function (error) {
                 let errorCode = error.code;
                 let errorMessage = error.message;
-                alert(errorMessage);
+                alert(errorCode, errorMessage);
             });
-    });
-
-    $(document).keypress(function(e) {
-        if (e.which == 13) {
-             $("#btn-login").trigger('click'); 
-        }
     });
 
     $("#login-gmail").click(function (event) {
         event.preventDefault();
         let provider = new firebase.auth.GoogleAuthProvider();
         signIn(provider);
-    })
+    });
 
     $("#login-facebook").click(function (event) {
         event.preventDefault();
         let provider = new firebase.auth.FacebookAuthProvider();
         signIn(provider);
-    })
+    });
 
-    function signIn (provider) {
+    function signIn(provider) {
         firebase.auth().signInWithPopup(provider)
-            .then(response =>{
-            window.location = 'profile.html?id=' + response.user.uid;
-            database.ref("users/" + response.user.uid).push({
-                name: name,
-                }); 
-        }).catch((error) => {
-            let errorCode = error.code;
-            let errorMessage = error.message;
-            let email = error.email;
-            let credential = error.credential;
-            alert(errorCode, errorMessage, email, credential)
-        })      
+            .then(response => {
+                window.location = 'profile.html?id=' + response.user.uid;
+                database.ref("users/" + response.user.uid).push({
+                    name: name,
+                });
+            }).catch((error) => {
+                let errorCode = error.code;
+                let errorMessage = error.message;
+                let email = error.email;
+                let credential = error.credential;
+                alert(errorCode, errorMessage, email, credential)
+            });
     }
-
-})
+});
 
